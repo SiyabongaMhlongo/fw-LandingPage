2e65a03c100a6fcf215264f9a96f3e3dd2abfda9
Siyabonga Mhlongo <75250613+SiyabongaMhlongo@users.noreply.github.com>
2021-04-19 08:32:14 +0200

    Add files via upload

---
+++ Landing Page/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    effect: 'fade', 
+    speed: 1000,
+  
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  
+    
+  });
+})
